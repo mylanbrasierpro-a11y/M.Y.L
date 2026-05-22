c5ffb6348e375a08ac2b80caf3b5730c6e20ac32
Mylan <mylanbrasierpro@gmial.com>
2026-05-22 11:08:00 +0200

    add scss form login and register

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichImageType;
@@ -13,16 +16,29 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('username')
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
+            ->add('password', PasswordType::class, [
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur',
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
             ->add('imageFile', VichImageType::class, [
                 'required' => false,
                 'allow_delete' => false,
                 'download_uri' => false,
+                'attr' => ['class' => 'form-input-file'],
+                'row_attr' => ['class' => 'form-group'],
             ])
         ;
-    }   
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
