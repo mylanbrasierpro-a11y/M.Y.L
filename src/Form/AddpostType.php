ae61dcc8efc3bbbbe59c11dcab12d1f3e7a63fe6
Mylan Brasier <mylanbrasierpro@gmail.com>
2026-06-02 14:16:15 +0200

    Change in overall style and stylization of forms partially finished

--- src/Form/AddpostType.php
+++ src/Form/AddpostType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Posts;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
@@ -14,8 +15,14 @@ class AddpostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+            ->add('title', TextType::class,[
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
+            ->add('Body', TextType::class,[
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
             ->add('imageFile', FileType::class,[
                 'required' => false,
                 'mapped' => true,
@@ -29,6 +36,8 @@ class AddpostType extends AbstractType
                             'image/jpg',
                             'image/webp',
                             'image/png',
+                            'attr' => ['class' => 'form-input'],
+                            'row_attr' => ['class' => 'form-group'],
 
 
                         ],
--- src/Form/UpdateType.php
+++ src/Form/UpdateType.php
@@ -8,6 +8,7 @@ use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,11 +18,19 @@ class UpdateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('body')
+           ->add('title', TextType::class,[
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
+            ->add('Body', TextType::class,[
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
             ->add('imageFile', FileType::class,[
                 'required' => false,
                 'mapped' => true,
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
             ])
         ;
     }
