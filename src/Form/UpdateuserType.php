2aafdac0b0504d3f1f3e63a5385890ca00d0b50b
Mylan <mylanbrasierpro@gmial.com>
2026-06-22 13:11:27 +0200

    bug

--- src/Form/UpdateuserType.php
+++ src/Form/UpdateuserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,10 +14,13 @@ class UpdateuserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class,[
+                'attr' => ['class' => 'form-input'],
+                'row_attr' => ['class' => 'form-group'],
+            ])
             ->add('imageFile', FileType::class,[
-                'required' => false,
                 'mapped' => true,
+                'label' => 'Image de profil',
             ])
         ;           
         ;
